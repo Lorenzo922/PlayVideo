4d4e90cae531861a84d3fc3d6c11917c75d15942
Yellowblack Studio <lorenzothurmermasi@gmail.com>
2025-07-19 18:30:11 -0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyA-PEvp2reLWNxI_HKPMS2vofxSNOvDzDE",
+  authDomain: "playvideos-e2d5d.firebaseapp.com",
+  databaseURL: "https://playvideos-e2d5d-default-rtdb.firebaseio.com",
+  projectId: "playvideos-e2d5d",
+  storageBucket: "playvideos-e2d5d.appspot.com",
+  messagingSenderId: "220622211507",
+  appId: "1:220622211507:web:920bc3199ffacb5b7963c6",
+  measurementId: "G-EJCD4QDK6G"
+};
+firebase.initializeApp(firebaseConfig);
